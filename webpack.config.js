ba9af493d2fe08b3f0ccda2e88fe3f3e357fcfb6
Edward Hernandez <edwardgta4@hotmail.com>
2023-01-20 16:48:49 -0500

    mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    mode: 'development',
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js',
-        publicPath: '/',
-    },
-    resolve:{
-        extensions: ['.js','.jsx'],
-        alias:{
-            components: path.resolve(__dirname,'src/components'),
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	mode: 'development',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+		alias: {
+			components: path.resolve(__dirname,'src/components/'),
             containers: path.resolve(__dirname, 'src/containers/'),
             pages: path.resolve(__dirname, 'src/pages/'),
             styles: path.resolve(__dirname, 'src/styles/'),
@@ -21,57 +21,49 @@ module.exports = {
             logos: path.resolve(__dirname, 'src/assets/logos/'),
             hooks: path.resolve(__dirname,'src/hooks/'),
             context: path.resolve(__dirname, './src/context/'),
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use:{
-                    loader:'babel-loader'
-                }
-            },
-            {
-                test: /\.html$/,
-                use:[
-                    {
-                        loader: 'html-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.(css|scss)$/,
-                use:[
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.(png|jp(e*)g|svg|gif)$/,
-                use:[
-                    "file-loader"
-                ]
-            },
-        ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        })
-    ],
-    devServer: { //Configuration for developing
-        static:{
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 3005,
-        historyApiFallback:true,
-    },
-    
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader",
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				type: 'asset'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+	],
+	devServer: {
+		historyApiFallback: true,
+	}
 }
